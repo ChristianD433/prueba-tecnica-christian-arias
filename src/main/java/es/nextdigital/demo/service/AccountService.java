aba46b9ec760feb54f31c16eb4f4b7b7fbb4ca50
Christian Arias <christiandaniel136@gmail.com>
2025-10-01 22:11:12 +0200

    feat(deposit): Implement deposit endpoint

--- src/main/java/es/nextdigital/demo/service/AccountService.java
+++ src/main/java/es/nextdigital/demo/service/AccountService.java
@@ -5,4 +5,6 @@ import org.springframework.data.domain.Page;
 
 public interface AccountService {
     Page<Transaction> getAccountMovements(Long accountId, Integer page, Integer size);
+
+    Double deposit(Long accountId, Long xCardId, Long xAtmCode, Double amount);
 }
